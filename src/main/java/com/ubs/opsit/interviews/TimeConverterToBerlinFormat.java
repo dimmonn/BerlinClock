253a439ebd6ab2715779f46ffe66d4dd28e0d7c7
Dmytro <nhfdf@ukr.net>
2016-03-22 15:33:37 +0100

    added platform independent line separator

--- src/main/java/com/ubs/opsit/interviews/TimeConverterToBerlinFormat.java
+++ src/main/java/com/ubs/opsit/interviews/TimeConverterToBerlinFormat.java
@@ -8,25 +8,28 @@ public class TimeConverterToBerlinFormat implements TimeConverter {
 	private static final Logger LOG = LoggerFactory.getLogger(TimeConverterToBerlinFormat.class);
 
 	private String getHours(int hours) {
+		String lineSeparator = System.getProperty("line.separator");
 		int[] topBottomTime = Helper.getTopBottomTimeValues(hours);
 		String fiveHoursBerlin = Helper.convertToBerlinForamtHelper("OOOO", "R", Optional.empty(), topBottomTime[0]);
 		String oneHourBerlin = Helper.convertToBerlinForamtHelper("OOOO", "R", Optional.empty(), topBottomTime[1]);
 		LOG.info("Calculated hours are: " + fiveHoursBerlin + " " + oneHourBerlin);
-		return fiveHoursBerlin + "\r\n" + oneHourBerlin + "\r\n";
+		return fiveHoursBerlin + lineSeparator + oneHourBerlin + lineSeparator;
 	}
 
 	private String getMinutes(int minutes) {
+		String lineSeparator = System.getProperty("line.separator");
 		int[] topBottomTime = Helper.getTopBottomTimeValues(minutes);
 		String minutesBerlinTop = Helper.convertToBerlinForamtHelper("OOOOOOOOOOO", "Y", Optional.of("R"),
 				topBottomTime[0]);
 		String minutesBerlinBottom = Helper.convertToBerlinForamtHelper("OOOO", "Y", Optional.empty(),
 				topBottomTime[1]);
-		LOG.info("Calculated minutes are: " + minutesBerlinTop + " " + minutesBerlinBottom.replace("\r\n", ""));
-		return minutesBerlinTop + "\r\n" + minutesBerlinBottom;
+		LOG.info("Calculated minutes are: " + minutesBerlinTop + " " + minutesBerlinBottom);
+		return minutesBerlinTop + lineSeparator + minutesBerlinBottom;
 	}
 
 	private String getSec(int sec) {
-		String berlinFormatSeconds = (sec % 2 == 0) ? "Y\r\n" : "O\r\n";
+		String lineSeparator = System.getProperty("line.separator");
+		String berlinFormatSeconds = (sec % 2 == 0) ? "Y" + lineSeparator : "O" + lineSeparator;
 		LOG.info("Calculated seconds are: " + berlinFormatSeconds);
 		return berlinFormatSeconds;
 
